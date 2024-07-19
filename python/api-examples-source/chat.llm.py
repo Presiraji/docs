0adaa3984452072fd3c51488a87f6f329a052910
Presiraji <143067071+Presiraji@users.noreply.github.com>
2024-07-18 21:52:03 -0400

    Update chat.llm.py

--- python/api-examples-source/chat.llm.py
+++ python/api-examples-source/chat.llm.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 import os
 
 # Set page configuration for a clean UI
-st.set_page_config(page_title="FRIDAY", layout="Center")
+st.set_page_config(page_title="FRIDAY", layout="centered")
 
 # Title and Disclaimer
 st.title("ChatGPT-like clone")
